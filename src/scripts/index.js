ff89dad20ace181107ca0b114c778e9f15831a67
Matthew Fung <mattfung94@gmail.com>
2021-03-02 21:53:10 +0000

    Add matchHeights script and debounce

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,8 @@
 import toggleMenu from './modules/menu';
-
+import debounce from './modules/debounce';
+import matchHeights from './modules/match-heights';
 document.addEventListener('DOMContentLoaded', () => {
   toggleMenu();
+  window.addEventListener("load", matchHeights, false);
+  window.addEventListener("resize", debounce(matchHeights, 100, false), false);  
 });
