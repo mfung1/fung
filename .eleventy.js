c4623c50d037bee85eff49b1d090b26440abb67f
Matthew Fung <mattfung94@gmail.com>
2021-03-02 17:47:54 +0000

    Add post template and attrs in md

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,23 @@
+// Import prior to `module.exports` within `.eleventy.js`
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  let markdownIt = require("markdown-it");
+  let markdownItAttrs = require("markdown-it-attrs");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
   eleventyConfig.addPassthroughCopy('src/images')
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "local",
+    }).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
